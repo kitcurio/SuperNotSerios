6a0ebb2cfda1f04b5c94332b4cd4778322e8f824
kitcurio <kasiarivers@gmail.com>
2024-05-07 10:41:24 -0400

    Initial Commit

---
+++ SuperNotSerios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperNotSerios
+//
+//  Created by Kasia Rivers on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperNotSerios/SuperNotSeriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperNotSeriosApp.swift
+//  SuperNotSerios
+//
+//  Created by Kasia Rivers on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperNotSeriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
